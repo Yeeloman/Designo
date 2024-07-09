a13b2ba8b748415163cb5c60882f508019856cc1
Yeeloman <yami.onlyme@gmail.com>
2024-07-09 12:52:21 +0100

    adding header to the layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,17 +4,18 @@
 	let { children } = $props();
 </script>
 
-<div class="flex h-[100px] w-full items-center justify-center bg-red-300">
+<div class="my-10 flex h-[100px] w-full items-center justify-center p-14">
 	<div class="w-[15%]"></div>
 	<div class="w-[30%] flex-grow">
-		<img class="" src={logoDark} alt="Designo Logo" />
+		<img class="w-[90%]" src={logoDark} alt="Designo Logo" />
 	</div>
 	<div class="w-4/6"></div>
 	<div class="flex w-[45%] space-x-7">
-		<a class="hover:underline" href="/about">OUR COMPANY</a>
-		<a class="hover:underline" href="/locations">LOCATONS</a>
-		<a class="hover:underline" href="/contact">CONTACT</a>
+		<a class="font-thin hover:underline hover:underline-offset-8" href="/about">OUR COMPANY</a>
+		<a class="font-thin hover:underline hover:underline-offset-8" href="/locations">LOCATONS</a>
+		<a class="font-thin hover:underline hover:underline-offset-8" href="/contact">CONTACT</a>
 	</div>
 	<div class="w-[15%]"></div>
 </div>
+
 {@render children()}

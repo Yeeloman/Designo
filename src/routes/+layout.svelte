1e83bed6a9ca7cc538124c9a9f50ceadc80947e4
Yeeloman <yami.onlyme@gmail.com>
2024-07-09 21:20:24 +0100

    latest changes for footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
 	import '../app.css';
 	import { Button } from '$lib/components/ui/button';
+	import { Separator } from '$lib/components/ui/separator';
 	import logoDark from '$assets/shared/desktop/logo-dark.png';
+	import logoLight from '$assets/shared/desktop/logo-light.png';
 	let { children } = $props();
 </script>
 
@@ -9,7 +11,7 @@
 	<title>designo: Home</title>
 </svelte:head>
 
-{#snippet header(logo, underlineOffset)}
+{#snippet header(logo, additionalClasses)}
 	<div class="flex h-[100px] w-[80%] items-center justify-between">
 		<div class="w-[20%]">
 			<a href="/">
@@ -17,21 +19,23 @@
 			</a>
 		</div>
 		<div class="flex w-auto space-x-7">
-			<a class="font-thin hover:underline {underlineOffset}" href="/about">OUR COMPANY</a>
-			<a class="font-thin hover:underline {underlineOffset}" href="/locations">LOCATONS</a>
-			<a class="font-thin hover:underline {underlineOffset}" href="/contact">CONTACT</a>
+			<a class="font-thin {additionalClasses}" href="/about">OUR COMPANY</a>
+			<a class="font-thin {additionalClasses}" href="/locations">LOCATONS</a>
+			<a class="font-thin {additionalClasses}" href="/contact">CONTACT</a>
 		</div>
 	</div>
 {/snippet}
 
 {#snippet footer()}
-	<div class="flex w-[80%] items-center justify-center rounded-2xl bg-[#E78168] p-16">
+	<!-- this is the get in touch section -->
+	<div class="top-0 flex w-[80%] items-center justify-center rounded-2xl bg-[#E78168] p-16">
 		<div class="w-3/4 font-semibold text-white">
 			<h1 class="m-5 w-[60%] text-4xl">Let’s talk about your project</h1>
 			<p class="m-5 w-[70%] text-sm">
 				Ready to take it to the next level? Contact us today and find out how our expertise can help
 				your business grow.
 			</p>
+			<p></p>
 		</div>
 		<div class="w-1/4">
 			<Button
@@ -40,11 +44,31 @@
 			>
 		</div>
 	</div>
+	<!-- this is the contact and social section -->
+	<div class="flex w-full flex-col items-center justify-center bg-[#1d1c1e] p-[5%]">
+		{@render header(logoLight, 'text-white')}
+		<Separator class="w-[80%] bg-gray-500" />
+		<div class="mt-10 flex w-[80%] items-center justify-between text-white">
+			<div>
+				<p>Designo Central Office</p>
+				<p>3886 Wellington Street</p>
+				<p>Toronto, Ontario M9C 3J5</p>
+			</div>
+			<div>
+				<p>Contact Us (Central Office)</p>
+				<p>P : +1 253-863-8967</p>
+				<p>M : contact@designo.co</p>
+			</div>
+			<div></div>
+		</div>
+	</div>
 {/snippet}
 
 <!-- main content -->
 <div class=" m-[2%] flex flex-col items-center justify-center">
-	{@render header(logoDark, 'underline-offset-8')}
+	{@render header(logoDark, 'hover:underline underline-offset-8')}
 	{@render children()}
+</div>
+<div class="relative flex flex-col items-center justify-center">
 	{@render footer()}
 </div>

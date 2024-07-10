552314a33bfd7e0d0eee48b4e7404d1d5659c5bb
Yeeloman <yami.onlyme@gmail.com>
2024-07-10 18:07:32 +0100

    almost done with the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,31 @@
 	import LaptopImage from '$assets/home/desktop/image-web-design-large.jpg';
 	import CoolSelfie from '$assets/home/desktop/image-app-design.jpg';
 	import GraphicDesign from '$assets/home/desktop/image-graphic-design.jpg';
+
+	import IllPassionate from '$assets/home/desktop/illustration-passionate.svg';
+	import IllResourceful from '$assets/home/desktop/illustration-resourceful.svg';
+	import IllFriendly from '$assets/home/desktop/illustration-friendly.svg';
+
+	const IllustratorElements = [
+		{
+			image: IllPassionate,
+			title: 'Passionate',
+			description:
+				'Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions.'
+		},
+		{
+			image: IllResourceful,
+			title: 'Resourceful',
+			description:
+				'Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs.'
+		},
+		{
+			image: IllFriendly,
+			title: 'friendly',
+			description:
+				'We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide.'
+		}
+	];
 </script>
 
 {#snippet AwardWinning(phoneImage)}
@@ -26,8 +51,8 @@
 	</div>
 {/snippet}
 
+<!-- Card section -->
 {#snippet Card(image, title)}
-	<!-- card section -->
 	<div
 		class="relative flex h-full w-full flex-col items-center justify-center rounded-lg border-none"
 	>
@@ -44,14 +69,33 @@
 	</div>
 {/snippet}
 
-<main class="flex flex-col items-center justify-center space-y-40">
-	{@render AwardWinning(HeroPhone)}
+<!-- Grid Div section -->
+{#snippet GridDiv()}
 	<div class="grid grid-cols-2 space-x-5">
 		{@render Card(LaptopImage, 'WEB')}
 		<div class="grid-col-1 grid">
 			{@render Card(CoolSelfie, 'APP')}
 			{@render Card(GraphicDesign, 'GRAPHIC')}
 		</div>
+	</div>
+{/snippet}
+
+{#snippet Illustrators(element)}
+	<div class="flex h-auto w-full flex-col items-center justify-center space-y-8 p-10">
+		<img src={element.image} alt="a passionate illustartor" />
+		<h1 class="text-1xl font-bold">{element.title}</h1>
+		<p class="text-balance text-center text-sm font-thin text-gray-700">
+			{element.description}
+		</p>
+	</div>
+{/snippet}
+<main class="flex flex-col items-center justify-center space-y-40">
+	{@render AwardWinning(HeroPhone)}
+	{@render GridDiv()}
+	<div class="flex h-auto w-full items-center justify-center p-10">
+		{#each IllustratorElements as element}
+			{@render Illustrators(element)}
+		{/each}
 	</div>
 </main>
 
